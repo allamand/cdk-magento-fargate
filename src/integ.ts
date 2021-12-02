049816835c43b3997fa21f5b141efbfb225ca522
EC2 Default User <ec2-user@ip-172-31-39-14.ap-south-1.compute.internal>
2021-12-02 08:58:34 +0000

    Fixed the Unit Testing

---
+++ src/integ.ts
@@ -0,0 +1,20 @@
+import {App}  from '@aws-cdk/core';
+import {MagentoStack} from './main';
+
+const stackName = process.env.CDK_STACK_NAME ? process.env.CDK_STACK_NAME : 'magento';
+const clusterName = stackName;
+
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+new MagentoStack(app, stackName, {
+  clusterName: clusterName,
+  createCluster: true,
+  description: "MY Description",
+  env: devEnv,
+});
+
+app.synth();
